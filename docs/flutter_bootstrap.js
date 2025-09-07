0cdc7b2475a3d4227f4fd1ae9ce6cbde5c882ebd
galgaloda <galgaloda@gmail.com>
2025-09-07 19:21:02 +0300

    Deploy enhanced student management system with class assignment

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"dd93de6fb1776398bf586cbd477deade1391c7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "300469486"
+    serviceWorkerVersion: "1258077722"
   }
 });
